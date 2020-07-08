fc47960982a359c852c385c6f6866a68272bfd6a
Asher Williams <asher.williams1998@gmail.com>
2020-07-08 15:59:56 -0500

    Created ProductController and have it list products, but validation still buggy

---
+++ StoreApplication/StoreApplication.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreApplication.Library;
+using StoreApplication.Library.Models;
+using StoreApplication.WebUI.ViewModels;
+
+namespace StoreApplication.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IProductRepository Repo { get; }
+
+        public ProductController(IProductRepository repo) =>
+            Repo = repo ?? throw new ArgumentNullException(nameof(repo));
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            IEnumerable<Product> products = Repo.GetAll();
+            IEnumerable<ProductViewModel> viewModels = products.Select(x => new ProductViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Price = x.Price,
+                AmountOrdered = 0
+            });
+
+            return View(viewModels);
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind("AmountOrdered")] IEnumerable<ProductViewModel> productsOrdered)
+        {
+            try
+            {
+                if(productsOrdered.Any(p => TryValidateModel(p) == false))
+                {
+                    IEnumerable<Product> products = Repo.GetAll();
+                    IEnumerable<ProductViewModel> viewModels = products.Select(x => new ProductViewModel
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Price = x.Price,
+                        AmountOrdered = 0
+                    });
+                    return View(viewModels);
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View(productsOrdered);
+            }
+        }
+    }
+}
---
+++ StoreApplication/StoreApplication.WebUI/ViewModels/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreApplication.WebUI.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Amount")]
+        [Range(0, int.MaxValue)]
+        [Required]
+        public int AmountOrdered { get; set; }
+    }
+}
