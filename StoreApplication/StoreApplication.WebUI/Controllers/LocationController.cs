708e6d12655bcd51ca9a808df0f9b4f7efac2c1e
Asher Williams <asher.williams1998@gmail.com>
2020-07-09 01:45:42 -0500

    Created Location Controller to where it has user select a location and then redirects them to products page.

---
+++ StoreApplication/StoreApplication.WebUI/Controllers/LocationController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreApplication.Library;
+using StoreApplication.Library.Models;
+using StoreApplication.WebUI.ViewModels;
+
+namespace StoreApplication.WebUI.Controllers
+{
+    public class LocationController : Controller
+    {
+        public ILocationRepository Repo { get; }
+
+        public LocationController(ILocationRepository repo) =>
+            Repo = repo ?? throw new ArgumentNullException(nameof(repo));
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult SelectLocation()
+        {
+            var customerId = TempData["currentCustomerID"];
+            TempData.Keep();
+            if (customerId == null)
+            {
+                return RedirectToAction("SignIn", "Customer");
+            }
+
+            IEnumerable<Location> locations = Repo.GetAll();
+            IEnumerable<LocationViewModel> viewModels = locations.Select(x => new LocationViewModel
+            {
+                Id = x.Id,
+                Address = x.Address,
+                City = x.City,
+                State = x.State
+            });
+
+            return View(viewModels);
+        }
+
+        
+        public ActionResult Select(int id)
+        {
+            var location = Repo.GetByID(id);
+            TempData["selectedLocation"] = location.Id;
+            return RedirectToAction("Create", "Product");
+        }
+    }
+}
